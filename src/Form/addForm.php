db6ffe32835a76f3120dd0060450462f99c60837
wolfhowling <blackwolfhowling@gmail.com>
2023-04-07 16:39:04 -0400

    Added new api system to send the password to a remote endpoint

--- src/Form/addForm.php
+++ src/Form/addForm.php
@@ -7,7 +7,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Messenger\MessengerTrait;
 use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\daily_password\dailyPasswordRepository;
+use Drupal\daily_password\DailyPasswordRepository;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class addForm implements FormInterface, ContainerInjectionInterface {
@@ -17,9 +17,9 @@ class addForm implements FormInterface, ContainerInjectionInterface {
 
   /**
    * Database repository
-   * @var dailyPasswordRepository
+   * @var DailyPasswordRepository
    */
-  protected dailyPasswordRepository $repository;
+  protected DailyPasswordRepository $repository;
 
 
   /**
@@ -51,10 +51,10 @@ class addForm implements FormInterface, ContainerInjectionInterface {
 
   /**
    * Constructor
-   * @param dailyPasswordRepository $repository
+   * @param DailyPasswordRepository $repository
    * @param AccountProxyInterface $current_user
    */
-  public function __construct(dailyPasswordRepository $repository, AccountProxyInterface $current_user) {
+  public function __construct(DailyPasswordRepository $repository, AccountProxyInterface $current_user) {
     $this->repository = $repository;
     $this->currentUser = $current_user;
   }
@@ -106,11 +106,59 @@ class addForm implements FormInterface, ContainerInjectionInterface {
       '#description' => $this->t('Select the frequency for the password update time frame.'),
     ];
 
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#button_type' => 'primary',
-      '#value' => $this->t('Add'),
-    ];
+      $form['remote'] = [
+          '#type' => 'details',
+          '#title' => $this->t('Remote submission'),
+      ];
+
+      $form['remote']['send'] = [
+          '#type' => 'checkbox',
+          '#title' => $this->t('Send to remote API.'),
+          '#default_value' => false,
+          '#description' => $this->t('Please select this checkbox to indicate whether you want to send the password to a remote API.'),
+      ];
+
+
+      $form['remote']['url'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('API Url'),
+          '#size' => 255,
+          '#description' => $this->t('Please provide the web address to the API endpoint, making sure to include the full http:// or https:// '),
+      ];
+
+
+      $form['remote']['header'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Header key'),
+          '#size' => 255,
+          '#description' => $this->t('Please specify the request header for the API token. If no header is provided, the default "x-api-token" will be used.'),
+      ];
+
+      $form['remote']['token'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('API Token'),
+          '#size' => 255,
+          '#description' => $this->t('Consider providing a security token in this field, as it is strongly recommended.'),
+      ];
+
+
+      $form['remote']['jsonkey'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('API JSON payload key'),
+          '#size' => 255,
+          '#description' => $this->t('Please specify the JSON key to be used for the endpoint. If no key is provided, the default key "password" will be used.'),
+      ];
+
+
+      $form['actions'] = [
+          '#type' => 'actions',
+      ];
+
+      $form['actions']['submit'] = [
+          '#type' => 'submit',
+          '#value' => $this->t('Add'),
+          '#button_type' => 'primary',
+      ];
 
     return $form;
   }
@@ -147,9 +195,14 @@ class addForm implements FormInterface, ContainerInjectionInterface {
     $account = $this->currentUser;
     // Save the submitted entry.
     $entry = [
-      'usernames' => $form_state->getValue('usernames'),
-      'frequency' => $form_state->getValue('frequency'),
-      'password' => t('Password not yet set'),
+        'usernames' => $form_state->getValue('usernames'),
+        'frequency' => $form_state->getValue('frequency'),
+        'password' => t('Password not yet set'),
+        'send' => $form_state->getValue('send'),
+        'url' => $form_state->getValue('url'),
+        'header' => $form_state->getValue('header'),
+        'token' => $form_state->getValue('token'),
+        'jsonkey' => $form_state->getValue('jsonkey'),
     ];
     $return = $this->repository->insert($entry);
     $form_state->setRedirect('daily_password.form_table');
